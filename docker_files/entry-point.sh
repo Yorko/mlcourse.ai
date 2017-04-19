88a5286e30dc42f4d5a43e1dde5f236496d1a68a
Yury Kashnitsky <yury.kashnitsky@gmail.com>
2017-04-19 12:19:16 +0300

    modify docker files

---
+++ docker_files/entry-point.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+echo "Command: $1"
+
+case $1 in
+  shell )
+    /bin/bash
+    ;;
+  h2o )
+    /usr/local/h2o
+    ;;
+  jupyter )
+    /usr/local/bin/jupyter notebook --ip="*" --no-browser --port 7777 --allow-root
+    ;;
+  zeppelin )
+    ;;
+  * )
+    echo "Unknown command $1, starting shell"
+    /bin/bash
+    ;;
+esac
