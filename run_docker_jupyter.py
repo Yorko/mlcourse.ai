361aa80fe41fe8f8bad5bf19964147f7fc9fef64
Stanislav Krasnoyarov <Stanislav.Krasnoyarov@gmail.com>
2017-09-10 16:57:21 +0300

    Fixed path with spaces issue

--- run_docker_jupyter.py
+++ run_docker_jupyter.py
@@ -7,7 +7,7 @@ def main():
     parser.add_argument("--net_host", action='store_true', help='Whether to use --net=host with docker run (for Linux servers)')
     args = parser.parse_args()
 
-    run_command = 'docker run -it {0} --rm -p 5022:22 -p 4545:4545 -v {1}:/notebooks -w /notebooks {2} jupyter'.format(
+    run_command = 'docker run -it {0} --rm -p 5022:22 -p 4545:4545 -v "{1}":/notebooks -w /notebooks {2} jupyter'.format(
         '--net=host' if args.net_host else '', os.getcwd(),  args.docker_tag)
 
     print('Running command\n' + run_command)
