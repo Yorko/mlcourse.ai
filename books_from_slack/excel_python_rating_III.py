1b9bab3c9f1aed322e857944134a17ef3c999fba
Zmeyoff Alexander <zmeyoff@amorous.com>
2018-02-20 18:06:12 +0300

    1802201806

--- books_from_slack/excel_python_rating_III.py
+++ books_from_slack/excel_python_rating_III.py
@@ -6,15 +6,15 @@ sheet_key="1HMn7uA8CwfBoWuDkiJkVomkKkoOVXyIm-vKFZlISotk"
 sheet_id="2026966465"
 service="gviz/tq?"
 #query="select * where A='Трунов Артем Геннадьевич'"
-#query="select * where A='Змеев Александр Викторович'"
-query="select * where A='Топорнин Дмитрий Дмитриевич'"
+query="select * where A='Змеев Александр Викторович'"
+#query="select * where A='Топорнин Дмитрий Дмитриевич'"
 payload = {'tqx':'out:csv',
            'sheet':sheet_id,
            'tq':query}
 encoded = urlencode(payload, quote_via=quote_plus)
 sheet_url = "/".join([base_url,sheet_key,service]) + encoded
 
-print (sheet_url)
+#print (sheet_url)
 r = requests.get(url=sheet_url)
 print(r.text)
 
