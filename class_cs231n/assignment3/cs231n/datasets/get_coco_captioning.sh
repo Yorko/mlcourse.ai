d02329377396d36fc6b8574440ee2da0dc990c90
Yorko <foofest@gmail.com>
2018-09-11 14:07:29 +0200

    .

--- class_cs231n/assignment3/cs231n/datasets/get_coco_captioning.sh
+++
@@ -1,3 +0,0 @@
-wget "http://cs231n.stanford.edu/coco_captioning.zip"
-unzip coco_captioning.zip
-rm coco_captioning.zip
--- class_cs231n/assignment3/cs231n/datasets/get_squeezenet_tf.sh
+++
@@ -1,3 +0,0 @@
-wget "http://cs231n.stanford.edu/squeezenet_tf.zip"
-unzip squeezenet_tf.zip
-rm squeezenet_tf.zip
