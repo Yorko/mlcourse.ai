9b3580bb512f7bdd80bff22603b9d8b30bedddc9
Vadim <31348740+vchulski@users.noreply.github.com>
2018-10-18 17:17:20 +0300

    Update run_docker_jupyter.py for Docker Toolbox users on Windows
    New function path - prepares the right path for both Linux and Windows. Have no ability to check on MacOS, but these changes shouldn't reflect.

--- run_docker_jupyter.py
+++ run_docker_jupyter.py
@@ -1,21 +1,36 @@
 import os
 import argparse
 
+def path(): 
+	''' Special case for windows docker toolbox users.'''
+	path = os.getcwd()
+	if os.getenv('DOCKER_TOOLBOX_INSTALL_PATH') is not None:
+		if len(os.environ['DOCKER_TOOLBOX_INSTALL_PATH'])>0:
+			path = path.replace(':', '')
+			path = path.replace('\\\\', '/')
+			path = path.replace('\\', '/')
+			path = path.replace('C', 'c')
+			path = '/'+path
+	else: 
+		path = '"'+path+'"'
+	return path
+
+path = path()
+
 def main():
     parser = argparse.ArgumentParser(add_help=True, description='Run docker image.')
     parser.add_argument("--docker_tag", "-t", default='festline/mlcourse_ai', help='Docker image tag')
     parser.add_argument("--net_host", action='store_true', help='Whether to use --net=host with docker run (for Linux servers)')
     args = parser.parse_args()
 
-    run_command = 'docker run -it {0} --rm -p 5022:22 -p 4545:4545 -v "{1}":/notebooks -w /notebooks {2} jupyter'.format(
-        '--net=host' if args.net_host else '', os.getcwd(),  args.docker_tag)
+    run_command = 'docker run -it {0} --rm -p 5022:22 -p 4545:4545 -v {1}:/notebooks -w /notebooks {2} jupyter'.format(
+        '--net=host' if args.net_host else '', path,  args.docker_tag)
 
     print('Running command\n' + run_command)
     os.system(run_command)
 
 if __name__ == '__main__':
     main()
-
 
 
 
