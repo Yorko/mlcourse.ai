65edbc92e97a471a95fc75e368069a7eeae9cc87
Artem <datamove@gmail.com>
2019-01-24 17:09:01 +0300

    added new docker scripts

---
+++ run_jupyter.cmd
@@ -0,0 +1,9 @@
+
+SET PWD=%cd%
+SET USER=%username%
+SET USER_ID=1000
+SET GROUP_ID=1000
+
+docker-compose -f docker\docker-compose.yaml up
+
+
---
+++ run_jupyter.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# this allows for container to be created and persisted.
+# which means that you can keep the changes you made, 
+# i.e. if you installed more software with pip.
+# 
+USER_ID=$(id -u) GROUP_ID=$(id -g) exec docker-compose -f docker/docker-compose.yaml up
