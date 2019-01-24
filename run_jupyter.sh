7dc62eed08cbf23da51a6f0b7a6becdd0ce688c3
Artem <datamove@gmail.com>
2019-01-24 18:06:58 +0300

    fix for failure to create existing group

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -4,4 +4,4 @@
 # which means that you can keep the changes you made, 
 # i.e. if you installed more software with pip.
 # 
-USER_ID=$(id -u) GROUP_ID=$(id -g) exec docker-compose -f docker/docker-compose.yaml up
+USER_ID=$(id -u) GROUP_ID=$(id -g) GROUP_NAME=$(id-gn) exec docker-compose -f docker/docker-compose.yaml up
