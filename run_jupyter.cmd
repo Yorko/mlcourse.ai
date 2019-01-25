0a1503013891520320a49311002cbb082d173e2d
Artem <datamove@gmail.com>
2019-01-26 00:39:12 +0300

    now using docker run and -u uid:gid format. No intermediate image created

--- run_jupyter.cmd
+++ run_jupyter.cmd
@@ -1,10 +1,18 @@
 
+
+SET IMAGE="datamove/mlcourse_ai"
+
+docker pull %IMAGE%
+
+docker run --rm -v %cd%:/notebooks -w /notebooks -e HOME=/notebooks/home -p 4545:8888 %IMAGE% jupyter-notebook --NotebookApp.ip=0.0.0.0 --NotebookApp.password_required=False --NotebookApp.token='' --NotebookApp.custom_display_url="http://localhost:4545"
+
+
 SET PWD=%cd%
 SET USER=%username%
 SET USER_ID=1000
 SET GROUP_ID=1000
 SET GROUP_NAME=fake
 
-docker-compose -f docker\docker-compose.yaml up
+REM docker-compose -f docker\docker-compose.yaml up
 
 
