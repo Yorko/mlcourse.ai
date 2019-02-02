29424e664a58dd22769917a73b994faee759743b
unknown <datamove@gmail.com>
2019-02-02 16:07:57 +0300

    fixed volume mounting in toolbox; added ip discovery in toolbox mode

--- run_jupyter.cmd
+++ run_jupyter.cmd
@@ -1,20 +1,26 @@
 
+REM
+REM Attention: Windows Toolbox users - you MUST put the course repo in C:\Users\%username\mlcourse.ai
+REM Attention: Use Ctrl-C to shut down. If you close the window, the container may still be running.
+REM 
 
 SET IMAGE="datamove/mlcourse_ai"
 
+REM check for new versin of the image
 docker pull %IMAGE%
 
 IF DEFINED  DOCKER_TOOLBOX_INSTALL_PATH (
-docker run -it --rm -u 1000:1000 -v /c/users/%username%/mlcourse.ai:/notebooks -w /notebooks -e HOME=/notebooks/home -p 4545:8888 %IMAGE% jupyter-notebook --NotebookApp.ip=0.0.0.0 --NotebookApp.password_required=False --NotebookApp.token='' --NotebookApp.custom_display_url="http://localhost:4545"
+FOR /F "tokens=* USEBACKQ" %%a in (`"docker-machine ip default"`) DO SET JUP_IP=%%a
+)
+
+IF DEFINED  DOCKER_TOOLBOX_INSTALL_PATH (
+
+docker run -it --rm -u 1000:1000 -v /c/Users/%username%/mlcourse.ai:/notebooks -w /notebooks -e HOME=/notebooks/home -p 4545:8888 %IMAGE% jupyter-notebook --NotebookApp.ip=0.0.0.0 --NotebookApp.password_required=False --NotebookApp.token='' --NotebookApp.custom_display_url=http://%JUP_IP%:4545
 
 ) ELSE (
 
 docker run -it --rm -u 1000:1000 -v %cd%:/notebooks -w /notebooks -e HOME=/notebooks/home -p 4545:8888 %IMAGE% jupyter-notebook --NotebookApp.ip=0.0.0.0 --NotebookApp.password_required=False --NotebookApp.token='' --NotebookApp.custom_display_url="http://localhost:4545"
 )
 
-REM
-REM Attention: Windows Toolbox users - you MUST put the course repo in C:\Users\%username\mlcourse.ai
-REM Attention: Windows Toolbox users - check your URL in the docker controls.
-REM Attention: Use Ctrl-C to shut down. If you close the window, the container may still run.
-REM 
+
 
